4de12e3a5c075c098a5c40ad5be36f151fab5b7f
Jon Oropeza <jon_oropeza@homedepot.com>
2020-03-28 10:19:42 -0700

    fail build

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Code Quality POC/i);
+  const linkElement = getByText(/Code repository/i);
   expect(linkElement).toBeInTheDocument();
 });
