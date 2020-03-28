9a9adcee6a94ce142512b532b16e6629cbea6a58
Jon Oropeza <jon_oropeza@homedepot.com>
2020-03-28 09:54:08 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-body">
+        <div>
+          <span role="img" aria-label="waving hand">👋</span>&nbsp;Code Quality POC
+        </div>
+        <div className="App-secondary">Code repository</div>
+      </section>
     </div>
   );
 }
